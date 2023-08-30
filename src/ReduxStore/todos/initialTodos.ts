46b21b7ef6df5922dd1b13ab68cfa42e0909542f
Kevin Nicolas <kncalderong@gmail.com>
2023-08-30 16:36:30 -0500

    feat: redux store scaffold setted

---
+++ src/ReduxStore/todos/initialTodos.ts
@@ -0,0 +1,22 @@
+import { TodoItem } from '@/types/data/todos'
+import { v4 as uuidv4 } from 'uuid'
+
+const initialTodos: TodoItem[] = [
+  {
+    id: uuidv4(),
+    text: 'take breakfast',
+    targetCycles: 1,
+  },
+  {
+    id: uuidv4(),
+    text: 'mobility routine',
+    targetCycles: 2,
+  },
+  {
+    id: uuidv4(),
+    text: 'English practice',
+    targetCycles: 1,
+  },
+]
+
+export default initialTodos
---
+++ src/ReduxStore/todos/todosSlice.ts
@@ -0,0 +1,50 @@
+import { createSlice, PayloadAction, createSelector } from '@reduxjs/toolkit'
+import initialTodos from './initialTodos'
+import { TodoItem } from '@/types/data/todos'
+import { RootState } from '@/ReduxStore'
+
+export interface TodosState {
+  todos: TodoItem[]
+  selectedTodoId: TodoItem['id'] | undefined
+}
+
+const initialState: TodosState = {
+  todos: initialTodos,
+  selectedTodoId: undefined,
+}
+
+export const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    setTodos: (state: TodosState, action: PayloadAction<TodoItem[]>) => {
+      state.todos = action.payload
+    },
+    addTodo: (state: TodosState, action: PayloadAction<TodoItem>) => {
+      state.todos.push(action.payload)
+    },
+    removeTodo: (state: TodosState, action: PayloadAction<TodoItem>) => {
+      state.todos.filter((todo) => todo.id !== action.payload.id)
+    },
+    selectTodo: (state: TodosState, action: PayloadAction<string>) => {
+      state.selectedTodoId = action.payload
+    },
+  },
+})
+
+export const { setTodos, addTodo, removeTodo, selectTodo } = todosSlice.actions
+
+//this is to memoize the selection of one Item, if the idToSelect or the item itself does not change, then the cached item is returned without filtering again
+export const getSelectedTodo = createSelector(
+  (state: RootState) => state.todos,
+  (todos) => {
+    if (todos.selectedTodoId) {
+      return todos.todos.find(
+        (todo: TodoItem) => todo.id === todos.selectedTodoId
+      )
+    }
+    return null
+  }
+)
+
+export default todosSlice.reducer
